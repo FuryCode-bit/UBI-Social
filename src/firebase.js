b90df80dd61df497ea65c7d0ba19e0538fdc57d1
Fury <62396294+FuryCode-bit@users.noreply.github.com>
2024-01-15 00:09:57 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,8 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDSU60H8KwJpr7ja1AXWNpRRPsTxC_wzWs",
-    authDomain: "ubi-social-5a9a6.firebaseapp.com",
-    projectId: "ubi-social-5a9a6",
-    storageBucket: "ubi-social-5a9a6.appspot.com",
-    messagingSenderId: "95497981078",
-    appId: "1:95497981078:web:b7ab6d701fcaa5d59506b7",
-    measurementId: "G-FD3ESEFDKF"
-  };
+// For Firebase JS SDK v7.20.0 and later
+const firebaseConfig = {};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
